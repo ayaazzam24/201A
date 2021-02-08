27439a3b9ec4b011b33b904a93165b3665901b47
ayaazzam24 <ayaazzam0101@gmail.com>
2021-02-08 16:05:30 +0200

    added new file

---
+++ js/app.js
@@ -0,0 +1,70 @@
+'use strict';
+let UserName = prompt('what is your name ?');
+
+
+let userInput = prompt('Do you think I like pasta ?');
+console.log(userInput.toLowerCase());
+ userInput = userInput.toLowerCase();
+if (userInput ==='yes' || userInput ==='y'){
+alert('correct')
+} else if(userInput ==='no' || userInput ==='n'){
+  alert('incorrect ')
+}
+
+
+let userinput = prompt('do you think I like Jawaker game ?');
+console.log(userinput.toLowerCase());
+ userinput = userinput.toLowerCase();
+if (userinput ==='yes' || userinput ==='y'){
+alert('correct')
+} else if(userinput ==='no' || userinput ==='n'){
+  alert('incorrect ')
+}
+
+
+let myFav = prompt('do you think I like PUBG game ?');
+console.log(myFav.toLowerCase());
+ myFav = myFav.toLowerCase();
+if (myFav ==='yes' || myFav ==='y'){
+alert('incorrect')
+} else if(myFav ==='no' || myFav ==='n'){
+  alert('correct ')
+}
+
+let myfav = prompt('do you think I LOVE SHOPPING ?');
+console.log(myfav.toLowerCase());
+ myfav = myfav.toLowerCase();
+if (myfav ==='yes' || myfav ==='y'){
+alert('correct')
+} else if(myfav ==='no' || myfav ==='n'){
+  alert('incorrect ')
+}
+
+let myuser = prompt('do you think my favourite color is black  ?');
+console.log(myuser.toLowerCase());
+ myuser = myuser.toLowerCase();
+if (myuser ==='yes' || myuser ==='y'){
+alert('correct')
+} else if(myuser ==='no' || myuser ==='n'){
+  alert('incorrect ')
+}
+
+let myUser = prompt('do you think i like study  ?');
+console.log(myUser.toLowerCase());
+ myUser = myUser.toLowerCase();
+if (myUser ==='yes' || myUser ==='y'){
+alert('incorrect')
+} else if(myUser ==='no' || myUser ==='n'){
+  alert('correct ')
+}
+
+alert ('welcome' + UserName)
+
+
+
+
+
+
+
+
+
