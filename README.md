ae8e0fb071fc89c812e9faca9ddd9bfd83e48fb6
ayaazzam24 <ayaazzam0101@gmail.com>
2021-02-09 20:03:04 +0200

    modified on files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-AboutMe
+# - About Me Guessing Game. 
+
+## - adding html a paragraph about me (orderd list  and unorderd list )
+
+## - adding a new features in javascript like if statement , for loop and used array add score  to count the right answer .
+
--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 'use strict';
+let score = 0
 let UserName = prompt('what is your name ?');
 
 
@@ -6,6 +7,7 @@ let userInput = prompt('Do you think I like pasta ?');
 //console.log(userInput.toLowerCase());
  userInput = userInput.toLowerCase();
 if (userInput ==='yes' || userInput ==='y'){
+score ++;
 alert('correct')
 } else if(userInput ==='no' || userInput ==='n'){
   alert('incorrect ')
@@ -16,6 +18,7 @@ let userLike = prompt('do you think I like Jawaker game ?');
 //console.log(userLike.toLowerCase());
  userLike = userLike.toLowerCase();
 if (userLike ==='yes' || userLike ==='y'){
+score ++;
 alert('correct')
 } else if(userLike ==='no' || userLike ==='n'){
   alert('incorrect ')
@@ -26,6 +29,7 @@ let myFav = prompt('do you think I like PUBG game ?');
 //console.log(myFav.toLowerCase());
  myFav = myFav.toLowerCase();
 if (myFav ==='yes' || myFav ==='y'){
+score++;
 alert('incorrect')
 } else if(myFav ==='no' || myFav ==='n'){
   alert('correct ')
@@ -35,6 +39,7 @@ let yourUser = prompt('do you think I LOVE SHOPPING ?');
 //console.log(yourUser.toLowerCase());
  yourUser = yourUser.toLowerCase();
 if (yourUser ==='yes' || yourUser ==='y'){
+score++;
 alert('correct')
 } else if(yourUser ==='no' || yourUser ==='n'){
   alert('incorrect ')
@@ -44,19 +49,60 @@ let myUser = prompt('do you think my favourite color is black  ?');
 //console.log(myUser.toLowerCase());
  myUser = myUser.toLowerCase();
 if (myUser ==='yes' || myUser ==='y'){
+score ++;
 alert('correct')
 } else if(myUser ==='no' || myUser ==='n'){
   alert('incorrect ')
 }
 
+let user1 = prompt('guess what is my age  ?');
+ parseInt(user1);
+if(user1===24){
+  score++;
+    alert ('correct my age is 24 ')
+}
+else{
+ for(let i=1 ; i<=4 ;i++){
+  if (user1 <= 23){
+    alert ('too low');
+    user1 = prompt('enter the correct number');  
+  }else if (user1 > 24){
+  alert ('too high');
+  user1=prompt ('enter the correct number')
+} if(user1 === 24){
+  score++;
+  break;
+ } 
+}
+alert ('you are correct')}
+
+
+
+let userFav =['black', 'blue' , 'green' ,'white','pink'];
+for (let i = 0; i < 6; i++) {
+  let userAnsw = prompt('Can you guess my favorite colors?');
+  let guess = userAnsw.toLowerCase();
+  for (let a = 0 ; a < userFav.length ; a++ ){
+    if (guess === userFav[a]) {
+      alert('Correct answer');
+       score++;
+      i = 6;
+      break;
+    }
+  }
+  if (i !== 6){
+    alert('Sorry wrong answer, try again');
+  }
+}
+
+ 
+
+alert('My favorite colors are: black, blue , green , white , pink');
+
+alert(' You got ' + score + ' correct answers!');
+
 
 alert ('welcome' + UserName)
-
-
-
-
-
-
 
 
 
